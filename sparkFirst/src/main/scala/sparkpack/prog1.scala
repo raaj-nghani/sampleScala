184c66766afd291f50efa0cd615f4ba13442dc33
Raj Bhardwaj <Raj Bhardwaj@DESKTOP-ULDT510>
2022-09-21 07:52:52 +0530

    rdd operation

---
+++ sparkFirst/src/main/scala/sparkpack/prog1.scala
@@ -0,0 +1,15 @@
+package sparkpack
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+object prog1 {
+	def main(args:Array[String]):Unit={
+			val conf = new SparkConf().setAppName("first program").setMaster("local[*]")
+					val sc = new SparkContext(conf)
+					sc.setLogLevel("ERROR")
+					val data = sc.textFile("file:///d:/data/txn")
+					data.foreach(println)
+					val gymdata = data.filter(x => x.contains("Gymnastics"))
+					gymdata.foreach(println)
+
+	}
+}
