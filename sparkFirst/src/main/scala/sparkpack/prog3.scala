3069d1473fa88ec5a09734bde0a87f525054f786
Raj Bhardwaj <Raj Bhardwaj@DESKTOP-ULDT510>
2022-09-27 09:17:02 +0530

    Random User

--- sparkFirst/src/main/scala/sparkpack/prog3.scala
+++ sparkFirst/src/main/scala/sparkpack/prog3.scala
@@ -2,6 +2,7 @@ package sparkpack
 import org.apache.spark._
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
+
 object prog3 {
   def main(args:Array[String]):Unit={
     val conf = new SparkConf().setAppName("check").setMaster("local[*]")
@@ -10,11 +11,35 @@ object prog3 {
     
     val spark = SparkSession.builder().getOrCreate()
     val date = current_date()
-    val df = spark.read.format("csv").option("header","true").load("file:///d:/data/abcde.txt")
+    val df = spark.read.format("json").option("multiline","true").load("file:///d:/data/random10.json")
     df.show()
     df.printSchema()
-    val df2 = df.withColumn("date",lit(date))
-    df2.show()
+    val flatdf= df.withColumn("results", expr("explode(results)"))
+    .select(
+    col("nationality"),
+    col("results.user.cell"),
+    col("results.user.dob"),
+    col("results.user.email"),
+    col("results.user.gender"),
+    col("results.user.location.*"),
+    col("results.user.md5"),
+    col("results.user.name.*"),
+    col("results.user.password"),
+    col("results.user.phone"),
+    col("results.user.picture.*"),
+    col("results.user.registered"),
+    col("results.user.salt"),
+    col("results.user.sha1"),
+    col("results.user.sha256"),
+    col("results.user.username"),
+    col("seed"),
+    col("version"))
+    flatdf.printSchema()
+    flatdf.show()
+   val df2 = flatdf.withColumn("date",lit(date))
+   df2.printSchema()
+   df2.show()
+   
   }
   
 }
